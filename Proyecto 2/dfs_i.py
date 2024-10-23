c6979a89d1ed4486936401f81be85ac2e272e6d4
amorac4 <amorac2024@cic.ipn.mx>
2024-10-23 16:12:34 -0600

    new file:   Proyecto 2/dfs_i.py

---
+++ Proyecto 2/dfs_i.py
@@ -0,0 +1,24 @@
+
+
+class DFS_ITERATIVO:
+
+    def __init__(self, graph):
+        self.graph
+
+    def buscar (self, nodo_inicio):
+
+        pila = [nodo_inicio]
+        visitados = set()
+
+        while pila:
+            nodo_actual = pila.pop()
+
+            if nodo_actual not in visitados:
+                visitados.add(nodo_actual)
+                print(f"Visitado: {nodo_actual.id}")
+
+                for arista in reversed(nodo_actual.aristas):
+                    vecino = arista.n2 if arista.n1 == nodo_actual else arista.n1
+                    if vecino not in visitados:
+                        pila.append(vecino)
+            return visitados
