e0425a54dfbc28d359945cc7390fbc3697430754
amorac4 <amorac2024@cic.ipn.mx>
2024-10-23 15:53:32 -0600

    new file:   .vscode/settings.json 	new file:   Proyecto 2/bfs.py

---
+++ Proyecto 2/bfs.py
@@ -0,0 +1,30 @@
+from collections import deque
+#from Sarista import Arista
+#from Snodo import Nodo
+#from Sgrafo import Grafo
+#from Generador import GenGrafo
+
+
+class BFS:
+
+    def __init__ (self graph):
+        self.graph = graph
+
+    def buscar(self, nodo_inicio):
+        #iniciar una cola para BFS
+        cola = deque([nodo_inicio])
+        #iniciar un conjunto para almacenar los nosos visitados
+        visitados = set([nodo_inicio])
+
+        while cola:
+            nodo_actual = cola.popleft()
+            print(f"Visitado: {nodo_actual.id}")
+
+            for arista in nodo_actual.aristas:
+                vecino = arista.n2 if arista.n2 == nodo_actual else arista.n1
+                if vecino not in visitados:
+                    visitados.add(vecino)
+                    cola.append(vecino)
+        return visitados
+
+
