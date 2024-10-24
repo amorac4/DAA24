013d1957a5587eb6308b357e4fcbdaccff8b73cc
amorac4 <amorac2024@cic.ipn.mx>
2024-10-24 10:29:17 -0600

    new file:   Proyecto 1/__init__.py 	new file:   Proyecto 2/__init__.py 	new file:   Proyecto 2/__pycache__/bfs.cpython-36.pyc 	new file:   Proyecto 2/__pycache__/dfs_i.cpython-36.pyc 	new file:   Proyecto 2/__pycache__/dfs_r.cpython-36.pyc 	modified:   Proyecto 2/busqueda.py

--- Proyecto 2/busqueda.py
+++ Proyecto 2/busqueda.py
@@ -1,8 +1,14 @@
+import sys
+import os
+
+proyecto_1_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'../proyecto 1'))
+if proyecto_1_path not in sys.path:
+   sys.path.append(proyecto_1_path)
+
 
 from bfs import BFS
 from dfs_r import DFS_Recursivo
 from dfs_i import DFS_ITERATIVO
-from Generador import GenGrafo
 from Sgrafo import grafoBarabasiAlbert, grafoDorogovtsevMendes, grafoErdosRenyi, grafoGeografico, grafoGilbert, grafoMalla
 
 
@@ -23,8 +29,40 @@ def generar_grafos():
     return grafos
 
 
-def 
+def iniciar_busqueda():
+    grafos = generar_grafos()
 
 
+    malla =[
+
+        ("Malla", grafoMalla(5,6)),
+        ("Malla", grafoMalla(10,10)),
+        ("Malla", grafoMalla(25,20)),
+    ]
+
+    grafos.extend(malla)
 
 
+    for nombre, grafo in grafos:
+        print(f"\nGrafo generado: {nombre} con {len(grafo.nodos)} nodos")
+        grafo.mostrat_grafo()
+
+        nodo_inicio = grafo.nodos[0] if grafo.nodos else None
+        if not nodo_inicio:
+            print(f"No hay nodos en el grafo {nombre}.")
+            continue
+
+        print("\nBusqueda en amplitud(BFS):")
+        bfs = BFS(grafo)
+        bfs.buscar(nodo_inicio)
+
+        print("\nBusqueda en profundidad Recursiva (DFS RECURSIVO):")
+        dfsR = DFS_Recursivo(grafo)
+        dfsR.buscar(nodo_inicio)
+
+        print("\nBusqueda en profundidad iterativa(BFS ITERATIVO):")
+        dfsI = DFS_ITERATIVO(grafo)
+        dfsI.buscar(nodo_inicio)
+
+        if __name__ == "__main__":
+            iniciar_busqueda()
